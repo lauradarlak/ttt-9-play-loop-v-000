9e45a1d47e7addd5097ac66deb484b1e3377fb02
Laura Darlak <laura@polyporedesign.com>
2018-08-04 18:03:24 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,7 +38,7 @@ end
 # Define your play method below
 
 def play(board)
-  turn(board)
-  until !board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  until board != [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    turn(board)
   end
 end
