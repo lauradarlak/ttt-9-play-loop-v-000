4186c258b942bdb0d4120fe3ef22fe03a60873c2
Laura Darlak <laura@polyporedesign.com>
2018-08-04 18:05:59 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -38,7 +38,7 @@ end
 # Define your play method below
 
 def play(board)
-  until board != [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    turn(board)
+  until turn(board) == 9
+    turn(board) += 1
   end
 end
